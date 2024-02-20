6314b43a74d0f777ed17e2cdfa9170680742e9f2
sodik abdullaev <sodiqabdullaev@mail.ru>
2024-02-20 11:02:12 +0000

    task 5 is complete

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express')
-const endpoints = require('./endpoints.json')
 const {
 	handlePsqlErrors,
 	handleCustomErrors,
 	handleServerErrors,
 } = require('./controllers/errors.controller')
 const { getTopics } = require('./controllers/topics.controller')
-const { getArticleById } = require('./controllers/articles.controller')
+const {
+	getArticleById,
+	getArticles,
+} = require('./controllers/articles.controller')
+const { getEndpoints } = require('./controllers/app.controller')
 
 const app = express()
-app.use(express.json())
 
 app.get('/api/topics', getTopics)
 
-app.get('/api', (req, res) => {
-	res.status(200).send({ endpoints })
-})
+app.get('/api', getEndpoints)
 
 app.get('/api/articles/:article_id', getArticleById)
+app.get('/api/articles', getArticles)
 
 app.use(handlePsqlErrors)
 app.use(handleCustomErrors)
